d278e80ce1e4d755263c502ac8b344666cf7baeb
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-29 21:21:43 -0400

    added blog post show screen

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
 
 const navigator = createStackNavigator(
   {
-    Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen
   },
   {
     initialRouteName: 'Index',
     defaultNavigationOptions: {
-      title: 'Blog'
+      title: 'Blogs'
     }
   }
 );
@@ -20,8 +22,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 }; 
