4cd92f393a792901d4f959cfe3df7c5c6da2a24e
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-07-05 13:45:39 -0400

    edit blog post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -33,5 +33,7 @@ const deleteBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
 	blogReducer,
   { addBlogPost, deleteBlogPost },
-	[],
+	[{ title: 'TEST POST', content: 'TEST CONTENT'}],
 );
+
+
